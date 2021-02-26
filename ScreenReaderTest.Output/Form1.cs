ebec9077c136f4ad5728a90d2eabca4390e44a93
needmoreraminbrain <needmoreraminbrain@gmail.com>
2021-02-26 16:38:02 +0000

    initial commit

---
+++ ScreenReaderTest.Output/Form1.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Windows.Forms;
+
+namespace ScreenReaderTest.Output
+{
+    public partial class Form1 : Form
+    {
+        private JavaAccessBridge _javaAccessBridge;
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            _javaAccessBridge = new JavaAccessBridge();
+            _javaAccessBridge.Initialize();
+        }
+    }
+}
---
+++ ScreenReaderTest.Output/JavaAccessBridge.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Runtime.InteropServices;
+
+namespace ScreenReaderTest.Output
+{
+    class JavaAccessBridge
+    {
+        public const string WINDOWS_ACCESS_BRIDGE = "WindowsAccessBridge-64.dll";
+
+        private MouseClickedDelegate _mouseClickDelegate;
+
+        public JavaAccessBridge()
+        {
+            _mouseClickDelegate = new MouseClickedDelegate(HandleMouseClick);
+        }
+
+        public void Initialize()
+        {
+            Windows_run();
+            setMouseClickedFP(_mouseClickDelegate);
+        }
+
+        private void HandleMouseClick(System.Int64 vmID, IntPtr jevent, IntPtr ac)
+        {
+            System.Diagnostics.Debug.WriteLine("this should output when it works instead of throwing an AccessViolationException");
+        }
+
+        [DllImport(WINDOWS_ACCESS_BRIDGE, SetLastError = true, ThrowOnUnmappableChar = true, CallingConvention = CallingConvention.Cdecl, CharSet = CharSet.Auto)]
+        private static extern void Windows_run();
+
+        [DllImport(WINDOWS_ACCESS_BRIDGE, SetLastError = true, ThrowOnUnmappableChar = true, CallingConvention = CallingConvention.Cdecl, CharSet = CharSet.Auto)]
+        private static extern void setMouseClickedFP([MarshalAs(UnmanagedType.FunctionPtr)]MouseClickedDelegate fp);
+
+        [UnmanagedFunctionPointer(CallingConvention.Cdecl)]
+        public delegate void MouseClickedDelegate(System.Int64 vmID, IntPtr jevent, IntPtr ac);
+    }
+}
